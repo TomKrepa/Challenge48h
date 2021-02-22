4da50c60f797a57a083225079c6179176c3a3899
Bjorn <52005620+TomKrepa@users.noreply.github.com>
2021-02-22 23:11:40 +0100

    Ajout de datetime

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('pages/home.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts/index.html')
 
 @app.route('/user')
 def user():
@@ -14,6 +19,10 @@ def user():
 @app.route('/about')
 def about():
     return render_template('pages/about.html')
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.now()}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
