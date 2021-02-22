8010cf37c57630bdb41bbf59846a76ce89ae99a0
Bjorn <52005620+TomKrepa@users.noreply.github.com>
2021-02-23 00:15:01 +0100

    Ajout de la database

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<Post "{}">' % self.title
 
 @app.route('/')
 def home():
@@ -25,4 +41,5 @@ def inject_now():
     return {'now': datetime.now()}
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True, port=5000)
